f3f479fb306db49423e946799634ee2a34a30541
jonknightfr <jon.knight@forgerock.com>
2025-09-12 13:30:19 +0100

    Location view

--- src/components/DelegatedAdmin.vue
+++ src/components/DelegatedAdmin.vue
@@ -169,7 +169,7 @@
                 <v-divider class="border-opacity-25"></v-divider>             
                 <v-card-text>
                     <template v-for="dev in user.deviceProfiles">
-                      LOC: {{ JSON.stringify(dev.location) }} <br>
+                      LOC: {{ JSON.stringify(JSON.parse(dev)["location"]) }} <br>
                     </template>
                 </v-card-text>
               </div>
